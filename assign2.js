041882a7a8a3f5bd8b7813000fde41a45554cf2e
abhi9621 <tewariabhishek9621@gmail.com>
2023-05-28 19:57:44 +0530

    yes

---
+++ assign2.js
@@ -0,0 +1,6 @@
+let person = {Name:"Abhishek",  age:21, address:"lucknow"};
+console.log(person)
+person.age=30
+person.email="abc"
+delete person.address
+console.log(person)
---
+++ index.js
@@ -0,0 +1,24 @@
+fruits=["Apple","banana","orange","grape"]
+fruits.push("kiwi")
+console.log(fruits)
+
+
+fruits.pop()
+console.log(fruits)
+
+// fruit1=[]
+fruits.unshift("lemon")
+fruit1=new Array(fruits)
+console.log(fruit1)
+console.log(fruits)
+
+fruits.shift()
+console.log(fruits)
+
+index =fruits.indexOf("orange")
+console.log(index)
+
+// citrus=[]
+console.log(fruit1)
+citrus=new Array(fruit1.slice(0,2))
+console.log(citrus)
